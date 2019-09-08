cfb6275df6707038c09ab95daccef789f0786bc2
rindy1123 <genryusinbaou@gmail.com>
2019-09-08 11:38:59 +0000

    seeds.rb変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,92 +9,92 @@
 
 require "csv"
  
-# # amazon prime 海外映画読み込み
-# CSV.foreach('db/movie_data/amazon_prime.csv', headers: true) do |row|
-#   Movie.create(title: row['タイトル'],
-#                   description: row['あらすじ'],
-#                   genre: row['ジャンル'],
-#                   app: 'amazon_prime',
-#                   movie_type: '海外映画'
-#                   )
-# end
+# amazon prime 海外映画読み込み
+CSV.foreach('db/movie_data/amazon_prime.csv', headers: true) do |row|
+  Movie.create(title: row['タイトル'],
+                  description: row['あらすじ'],
+                  genre: row['ジャンル'],
+                  app: 'amazon_prime',
+                  movie_type: '海外映画'
+                  )
+end
 
-# # hulu海外映画読み込み
-# CSV.foreach('db/movie_data/hulu_scraping.csv', headers: true) do |row|
-#   Movie.create(title: row['タイトル'],
-#                   description: row['あらすじ'],
-#                   genre: row['ジャンル'],
-#                   app: 'hulu',
-#                   movie_type: '海外映画'
-#                   )
-# end
+# hulu海外映画読み込み
+CSV.foreach('db/movie_data/hulu_scraping.csv', headers: true) do |row|
+  Movie.create(title: row['タイトル'],
+                  description: row['あらすじ'],
+                  genre: row['ジャンル'],
+                  app: 'hulu',
+                  movie_type: '海外映画'
+                  )
+end
 
-# # Netflix海外映画読み込み
-# CSV.foreach('db/movie_data/netflix_scraping.csv', headers: true) do |row|
-#   Movie.create(title: row['タイトル'],
-#                   description: row['あらすじ'],
-#                   genre: row['ジャンル'],
-#                   app: 'netflix',
-#                   movie_type: '海外映画'
-#                   )
-# end
+# Netflix海外映画読み込み
+CSV.foreach('db/movie_data/netflix_scraping.csv', headers: true) do |row|
+  Movie.create(title: row['タイトル'],
+                  description: row['あらすじ'],
+                  genre: row['ジャンル'],
+                  app: 'netflix',
+                  movie_type: '海外映画'
+                  )
+end
 
-# # amazon prime 日本映画読み込み
-# CSV.foreach('db/movie_data/amazon_prime_japanese.csv', headers: true) do |row|
-#   Movie.create(title: row['タイトル'],
-#                   description: row['あらすじ'],
-#                   genre: row['ジャンル'],
-#                   app: 'amazon_prime',
-#                   movie_type: '日本映画'
-#                   )
-# end
+# amazon prime 日本映画読み込み
+CSV.foreach('db/movie_data/amazon_prime_japanese.csv', headers: true) do |row|
+  Movie.create(title: row['タイトル'],
+                  description: row['あらすじ'],
+                  genre: row['ジャンル'],
+                  app: 'amazon_prime',
+                  movie_type: '日本映画'
+                  )
+end
 
-# # hulu日本映画読み込み
-# CSV.foreach('db/movie_data/hulu_japanese_scraping.csv', headers: true) do |row|
-#   Movie.create(title: row['タイトル'],
-#                   description: row['あらすじ'],
-#                   genre: row['ジャンル'],
-#                   app: 'hulu',
-#                   movie_type: '日本映画'
-#                   )
-# end
+# hulu日本映画読み込み
+CSV.foreach('db/movie_data/hulu_japanese_scraping.csv', headers: true) do |row|
+  Movie.create(title: row['タイトル'],
+                  description: row['あらすじ'],
+                  genre: row['ジャンル'],
+                  app: 'hulu',
+                  movie_type: '日本映画'
+                  )
+end
 
-# # Netflix日本映画読み込み
-# CSV.foreach('db/movie_data/netflix_japanese_scraping.csv', headers: true) do |row|
-#   Movie.create(title: row['タイトル'],
-#                   description: row['あらすじ'],
-#                   genre: row['ジャンル'],
-#                   app: 'netflix',
-#                   movie_type: '日本映画'
-#                   )
-# end
+# Netflix日本映画読み込み
+CSV.foreach('db/movie_data/netflix_japanese_scraping.csv', headers: true) do |row|
+  Movie.create(title: row['タイトル'],
+                  description: row['あらすじ'],
+                  genre: row['ジャンル'],
+                  app: 'netflix',
+                  movie_type: '日本映画'
+                  )
+end
 
-# # amazon prime アニメ読み込み
-# CSV.foreach('db/movie_data/amazon_prime_anime.csv', headers: true) do |row|
-#   Movie.create(title: row['タイトル'],
-#                   description: row['あらすじ'],
-#                   genre: row['ジャンル'],
-#                   app: 'amazon_prime',
-#                   movie_type: 'アニメ'
-#                   )
-# end
+# amazon prime アニメ読み込み
+CSV.foreach('db/movie_data/amazon_prime_anime.csv', headers: true) do |row|
+  Movie.create(title: row['タイトル'],
+                  description: row['あらすじ'],
+                  genre: row['ジャンル'],
+                  app: 'amazon_prime',
+                  movie_type: 'アニメ'
+                  )
+end
 
-# # huluアニメ読み込み
-# CSV.foreach('db/movie_data/hulu_anime_scraping.csv', headers: true) do |row|
-#   Movie.create(title: row['タイトル'],
-#                   description: row['あらすじ'],
-#                   genre: row['ジャンル'],
-#                   app: 'hulu',
-#                   movie_type: 'アニメ'
-#                   )
-# end
+# huluアニメ読み込み
+CSV.foreach('db/movie_data/hulu_anime_scraping.csv', headers: true) do |row|
+  Movie.create(title: row['タイトル'],
+                  description: row['あらすじ'],
+                  genre: row['ジャンル'],
+                  app: 'hulu',
+                  movie_type: 'アニメ'
+                  )
+end
 
-# # Netflixアニメ読み込み
-# CSV.foreach('db/movie_data/netflix_anime_scraping.csv', headers: true) do |row|
-#   Movie.create(title: row['タイトル'],
-#                   description: row['あらすじ'],
-#                   genre: row['ジャンル'],
-#                   app: 'netflix',
-#                   movie_type: 'アニメ'
-#                   )
-# end
+# Netflixアニメ読み込み
+CSV.foreach('db/movie_data/netflix_anime_scraping.csv', headers: true) do |row|
+  Movie.create(title: row['タイトル'],
+                  description: row['あらすじ'],
+                  genre: row['ジャンル'],
+                  app: 'netflix',
+                  movie_type: 'アニメ'
+                  )
+end
